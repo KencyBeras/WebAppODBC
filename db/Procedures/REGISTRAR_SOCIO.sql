42f77d06b2c21389cd8db15623bb5f75442d8642
Hatzer <juanpfederico@gmail.com>
2018-09-11 20:33:48 -0300

    Commit en procedure (se debe utilizar odbc_num_rows para ver si impactó en la BD

--- db/Procedures/REGISTRAR_SOCIO.sql
+++ db/Procedures/REGISTRAR_SOCIO.sql
@@ -7,25 +7,24 @@ CREATE PROCEDURE REGISTRAR_SOCIO(
     IN s_apellido VARCHAR(50),
     IN s_direccion VARCHAR(70),
     IN s_telefono VARCHAR(15),
-    IN s_email VARCHAR(45),
-    OUT respuesta INT
+    IN s_email VARCHAR(45)
 )
 COMMENT 'Da de alta un socio con un numero de afiliado único'
 BEGIN
 	-- Declaraciones
 	DECLARE error_registro CONDITION FOR SQLSTATE '22012';
 	DECLARE s_numafiliado INT DEFAULT 0; -- NUM_AFILIADO QUE SE OTORGA AL NUEVO USUARIO
-    SET respuesta = 0;
     -- Validaciones
     SELECT IFNULL(MAX(num_afiliado)+1, 0) INTO s_numafiliado from socio;
     IF(s_numafiliado>0) THEN
 		INSERT INTO socio (num_afiliado, user, pass, nombre, apellido, direccion, telefono, email)
 		VALUES (s_numafiliado, s_user, s_pass, s_nombre, s_apellido, s_direccion, s_telefono, s_email);
-        SET respuesta = 1;
 	ELSE
 		SIGNAL SQLSTATE '45000'
 		SET MESSAGE_TEXT = 'Error al dar de alta el socio';
 	END IF;
+    
+COMMIT;
 END
 //
 DELIMITER ;
