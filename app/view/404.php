85b127ec3c368e2860760bcce35726813606c69f
Hatzer <juanpfederico@gmail.com>
2018-09-11 19:26:04 -0300

    404 con bootstrap

---
+++ app/view/404.php
@@ -0,0 +1,26 @@
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<link href="../../public/css/404.css" rel="stylesheet" id="404">
+
+<!-- Cuerpo -->
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="error-template">
+                <h1>
+                    Problemas!</h1>
+                <h2>
+                    404 - Página no encontrada</h2>
+                <div class="error-details">
+                    Lo sentimos, la URL indicada no se encuentra disponible..
+                </div>
+                <div class="error-actions">
+                    <a href="../../" class="btn btn-primary btn-lg"><span class="glyphicon glyphicon-home"></span>
+                        Volver al inicio </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
