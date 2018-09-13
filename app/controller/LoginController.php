8cc5858c80ec825388159524f19984ad717954ab
Hatzer <juanpfederico@gmail.com>
2018-09-13 17:22:56 -0300

    Cambio tiempo de sesión a 30 minutos

--- app/controller/LoginController.php
+++ app/controller/LoginController.php
@@ -21,6 +21,9 @@ $socio = $socioDao->loginSocio($user, $pass);
 if(isset($socio)){ //Si coincide un socio con el solicitado en el login
 	$_SESSION["datosSesion"] = json_encode($socio); //Lo agrego como una variable de sesión para utilizar en el resto de la misma
 	$_SESSION["tipoSesion"] = "socio";
+	//Seteo el tiempo de sesión en 30 minutos (60*30 = 1800), que se refresca mediante cada @session_start()
+	ini_set("session.cookie_lifetime","1800");
+	ini_set("session.gc_maxlifetime","1800");
 }
 else $_SESSION["errorLogin"] = "Usuario o contraseña incorrectos";
 header("Location: ../"); //Redirecciono al index.php que maneja las sesiones
