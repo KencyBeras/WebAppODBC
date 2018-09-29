2e0574f3b12a48eec97d7bb3130ea59b42ddbf53
Andrés Filoso <andresfilosok@gmail.com>
2018-09-29 02:03:12 -0300

    No more fixes por hoy.

--- app/view/verSede.php
+++ app/view/verSede.php
@@ -48,7 +48,7 @@ $localidad = explode(' ', $sede)[0]; //Primera palabra de la localidad
 <body class="fix-header card-no-border">
     <!-- ============================================================== -->
     <!-- Preloader - style you can find in spinners.css -->
-    <!-- ============================================================== -->
+    <!-- ============================================================== 
     <div class="preloader">
         <svg class="circular" viewBox="25 25 50 50">
             <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" /> </svg>
@@ -111,14 +111,16 @@ $localidad = explode(' ', $sede)[0]; //Primera palabra de la localidad
 
 
                                         <div class="map-box">
-                                          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d470029.1604841957!2d72.29955005258641!3d23.019996818380896!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e848aba5bd449%3A0x4fcedd11614f6516!2sAhmedabad%2C+Gujarat!5e0!3m2!1sen!2sin!4v1493204785508" width="100%" height="280" frameborder="0" style="border:0" allowfullscreen></iframe>
+                                          
+                                          <?php require('../../public/maps/' . $sede . 'Map.html') ?>
 
-                                        <br>
-                                        <br>
-                                          <a href="reservar.php?id=<?php echo $idFilial;?>&sede=<?php echo $localidad ?>" class="btn btn-primary">Reservar</a>
+                                        
+                                          
                                     </div>
 
                                   </div>
+                                    <br>
+                                  <a href="reservar.php?id=<?php echo $idFilial;?>&sede=<?php echo $localidad ?>" class="btn btn-primary">Reservar</a>
 
 
                                 </div>
